7391135e5608a4eb9f65ec0086a837a875653abc
gwkim <gwk1231@naver.com>
2025-04-10 16:55:14 +0900

    Login_Layout

--- app/src/main/java/com/example/login_layout/MainActivity.java
+++ app/src/main/java/com/example/login_layout/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.login_layout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    public TextView inputID, inputPS, setView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,18 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        inputID = findViewById(R.id.inputID);
+        inputPS = findViewById(R.id.inputPS);
+        setView = findViewById(R.id.setView);
+    }
+
+
+    public void onclick(View view) {
+
+        String id = inputID.getText().toString();
+        String password = inputPS.getText().toString();
+        setView.setText("회원가입이 완료되었습니다."+ "\n아이디: "+ id + "\n패스워드: "+password);
+
     }
 }
